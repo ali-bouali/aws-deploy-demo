1053ff41bd8b8222e73949f59d07e666e382a9ff
ali-bouali <bouali.pro@gmail.com>
2023-09-09 19:00:25 +0200

    init project

---
+++ src/main/java/com/alibou/aws/AwsDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alibou.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeployDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeployDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alibou/aws/AwsDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibou.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeployDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
